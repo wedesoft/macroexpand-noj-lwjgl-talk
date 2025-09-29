370b853afd0d5f5b43011f7bab784e1420d1c9ee
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-09-29 19:54:24 +0100

    Add vertex and fragment shader slide

--- quad.clj
+++ quad.clj
@@ -14,6 +14,28 @@
 (GLFW/glfwMakeContextCurrent window)
 (GL/createCapabilities)
 
+(def vertex-source "
+#version 130
+
+in vec3 point;
+
+void main()
+{
+  gl_Position = vec4(point, 1);
+}")
+
+(def fragment-source "
+#version 130
+
+uniform vec2 iResolution;
+out vec4 fragColor;
+
+void main()
+{
+  vec2 uv = gl_FragCoord.xy / iResolution.xy;
+  fragColor = vec4(uv, 0, 1);
+}")
+
 (while (not (GLFW/glfwWindowShouldClose window))
        (GL11/glClearColor 0.0 1.0 0.0 1.0)
        (GL11/glClear GL11/GL_COLOR_BUFFER_BIT)
