848850317845d8d22a512162e4e505457f0c3e0d
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-09-29 19:57:14 +0100

    Compiling shaders

--- quad.clj
+++ quad.clj
@@ -36,6 +36,16 @@ void main()
   fragColor = vec4(uv, 0, 1);
 }")
 
+(defn make-shader [source shader-type]
+  (let [shader (GL20/glCreateShader shader-type)]
+    (GL20/glShaderSource shader source)
+    (GL20/glCompileShader shader)
+    (when (zero? (GL20/glGetShaderi shader GL20/GL_COMPILE_STATUS))
+      (throw (Exception. (GL20/glGetShaderInfoLog shader 1024))))
+    shader))
+
+(def vertex-shader (make-shader vertex-source GL20/GL_VERTEX_SHADER))
+(def fragment-shader (make-shader fragment-source GL20/GL_FRAGMENT_SHADER))
 (while (not (GLFW/glfwWindowShouldClose window))
        (GL11/glClearColor 0.0 1.0 0.0 1.0)
        (GL11/glClear GL11/GL_COLOR_BUFFER_BIT)
