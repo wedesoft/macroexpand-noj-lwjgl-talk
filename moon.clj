172da1ecbf69ddab21bd7201a8cc562a021ab650
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-10-01 21:41:07 +0100

    Add horizon matrix

--- moon.clj
+++ moon.clj
@@ -80,6 +80,33 @@ vec2 uv(vec3 p)
   return vec2(u, v);
 }")
 
+(def oriented-matrix-source "
+#version 130
+
+vec3 orthogonal_vector(vec3 n)
+{
+  vec3 b;
+  if (abs(n.x) <= abs(n.y)) {
+    if (abs(n.x) <= abs(n.z))
+      b = vec3(1, 0, 0);
+    else
+      b = vec3(0, 0, 1);
+  } else {
+    if (abs(n.y) <= abs(n.z))
+      b = vec3(0, 1, 0);
+    else
+      b = vec3(0, 0, 1);
+  };
+  return normalize(cross(n, b));
+}
+
+mat3 oriented_matrix(vec3 n)
+{
+  vec3 o1 = orthogonal_vector(n);
+  vec3 o2 = cross(n, o1);
+  return mat3(n, o1, o2);
+}")
+
 (def color-source "
 #version 130
 
